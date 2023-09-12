d37490124572064bc3222927a63a5b836ce159a8
Adenilson Ribeiro <134528770+aaadjrbr@users.noreply.github.com>
2023-09-12 17:44:08 -0300

    updating-123

--- general-js/app.js
+++ general-js/app.js
@@ -1,19 +1,3 @@
-// app.js
-
-// Function to update the test results or comments
-function updateTestResults(data) {
-  const testResultsContainer = document.getElementById('test-results');
-
-  // Check if data is available
-  if (data && data.results) {
-      // Display the retrieved data in the test results container
-      testResultsContainer.innerHTML = `Data from the database: ${data.results}`;
-  } else {
-      // Display a test message or comment if data is not available
-      testResultsContainer.innerHTML = 'Test successful - Site is connected to the database!';
-  }
-}
-
 // Fetch data from the serverless function
 fetch('../netlify/functions/planetScaleQuery.js')
   .then((response) => response.json())
