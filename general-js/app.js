8fb508cb4a77b7284a80a3d2110d32f6cf679a82
Adenilson Ribeiro <134528770+aaadjrbr@users.noreply.github.com>
2023-09-12 17:33:04 -0300

    test111

--- general-js/app.js
+++ general-js/app.js
@@ -1,5 +1,26 @@
-fetch('/..netlify/functions/planetScaleQuery')
+// app.js
+
+// Function to update the test results or comments
+function updateTestResults(data) {
+  const testResultsContainer = document.getElementById('test-results');
+
+  // Check if data is available
+  if (data && data.results) {
+      // Display the retrieved data in the test results container
+      testResultsContainer.innerHTML = `Data from the database: ${data.results}`;
+  } else {
+      // Display a test message or comment if data is not available
+      testResultsContainer.innerHTML = 'Test successful - Site is connected to the database!';
+  }
+}
+
+// Fetch data from the serverless function
+fetch('../netlify/functions/planetScaleQuery.js')
   .then((response) => response.json())
   .then((data) => {
-    console.log(data); // Do something with the database query results
+      // Call the function to update the test results or comments
+      updateTestResults(data);
+  })
+  .catch((error) => {
+      console.error(error); // Log any errors that occur during the fetch request
   });
