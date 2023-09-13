459f2e92a758887642926f2f5ee51fa3807802f4
Adenilson Ribeiro <134528770+aaadjrbr@users.noreply.github.com>
2023-09-13 14:25:52 -0300

    add-psandauth0

--- general-js/app.js
+++ general-js/app.js
@@ -1,10 +1,33 @@
-// Fetch data from the serverless function
-fetch('../netlify/functions/planetScaleQuery.js')
-  .then((response) => response.json())
-  .then((data) => {
+// Import and initialize Auth0
+import { Auth0Client } from '@auth0/auth0-spa-js';
+
+const auth0 = new Auth0Client({
+  domain: 'dev-guf031gyn3qsfpkw.us.auth0.com',
+  client_id: 'btLuY70GxivGYTGKVUxh0XJLmgqB4xqV',
+  redirect_uri: 'https://adenilsonribeiro-cursos.cloud/bem-vindo', // Replace with your callback URL
+});
+
+// Function to fetch data from the serverless function
+async function fetchData() {
+  try {
+    const authToken = await auth0.getIdToken(); // Get the user's Auth0 ID token
+    const response = await fetch('/.netlify/functions/planetScaleQuery', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${authToken}`,
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
       // Call the function to update the test results or comments
       updateTestResults(data);
-  })
-  .catch((error) => {
-      console.error(error); // Log any errors that occur during the fetch request
-  });
+    } else {
+      throw new Error('Failed to fetch data');
+    }
+  } catch (error) {
+    console.error(error); // Log any errors that occur during the fetch request
+  }
+}
+
+// Call the fetchData function when needed, such as after user authentication
--- netlify/functions/planetScaleQuery.js
+++ netlify/functions/planetScaleQuery.js
@@ -1,20 +1,45 @@
-// Import the required libraries
 import { connect } from '@planetscale/database';
+import { Auth0Client } from '@auth0/auth0-spa-js';
 
 export async function handler(event, context) {
   try {
+    // Check if the request includes an authentication token (JWT) from Auth0
+    const authToken = event.headers.authorization;
+
+    if (!authToken) {
+      return {
+        statusCode: 401,
+        body: JSON.stringify({ error: 'Unauthorized' }),
+      };
+    }
+
+    // Initialize Auth0 and validate the token
+    const auth0 = new Auth0Client({
+      domain: 'dev-guf031gyn3qsfpkw.us.auth0.com',
+      client_id: 'btLuY70GxivGYTGKVUxh0XJLmgqB4xqV',
+    });
+
+    const user = await auth0.getUser(authToken);
+
+    if (!user) {
+      return {
+        statusCode: 401,
+        body: JSON.stringify({ error: 'Invalid token' }),
+      };
+    }
+
     // Define your PlanetScale database connection details
     const config = {
-        host: 'aws.connect.psdb.cloud',     // Replace with your actual database host
-        username: '3w9lke9ua0wg8wj2mc1r',   // Replace with your actual database username
-        password: 'main-2023-09-12-41ym2o', // Replace with your actual database password
-      };
-      
+      host: 'aws.connect.psdb.cloud',     // Replace with your actual database host
+      username: '3w9lke9ua0wg8wj2mc1r',         // Replace with your actual database username
+      password: 'main-2023-09-12-41ym2o',         // Replace with your actual database password
+    };
+
     // Connect to the database
     const conn = connect(config);
 
-    // Execute a sample query (you can replace this with your actual query)
-    const results = await conn.execute('SELECT 1 FROM dual WHERE 1 = ?', [1]);
+    // Execute your PlanetScale query
+    const results = await conn.execute('SELECT * FROM your_table');
 
     // Close the database connection
     await conn.close();
