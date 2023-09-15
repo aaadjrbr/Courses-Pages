d3399d118383640a64ec24db0bdef755a8b02a16
Adenilson Ribeiro <134528770+aaadjrbr@users.noreply.github.com>
2023-09-15 10:03:37 -0300

    update3-firebase-google

--- general-js/app.js
+++ general-js/app.js
@@ -1,61 +1,34 @@
-// Import Firebas
-
+// Import Firebase
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 
-
-// Your Firebase configuration
+// Your Firebase configuration (same as login and registration pages)
 const firebaseConfig = {
-    apiKey: "AIzaSyAtq3i3emo88rNL90C5JMZMwGmgc617p9I",
-    authDomain: "english-courses-adenilson.firebaseapp.com",
-    projectId: "english-courses-adenilson",
-    storageBucket: "english-courses-adenilson.appspot.com",
-    messagingSenderId: "1047482281392",
-    appId: "1:1047482281392:web:5a8e7b2fd0874bbd0afa75",
-    measurementId: "G-5TRGBP6V13"
-  };
+  apiKey: "AIzaSyAtq3i3emo88rNL90C5JMZMwGmgc617p9I",
+  authDomain: "english-courses-adenilson.firebaseapp.com",
+  projectId: "english-courses-adenilson",
+  storageBucket: "english-courses-adenilson.appspot.com",
+  messagingSenderId: "1047482281392",
+  appId: "1:1047482281392:web:5a8e7b2fd0874bbd0afa75",
+  measurementId: "G-5TRGBP6V13"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Get the Firebase Auth instance
 const auth = getAuth();
 
 // Function to sign in with Email/Password
-const handleSignInWithEmailAndPassword = () => {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // User signed in successfully
-            const user = userCredential.user;
-            console.log('User signed in:', user);
-
-            // Redirect the user to a different page
-            window.location.href = './bem-vindo.html'; // Replace with the desired destination
-        })
-        .catch((error) => {
-            // Handle sign-in errors
-            console.error('Error signing in:', error);
-        });
+const signInWithEmailAndPassword = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
 };
 
-// Function to sign in with Google
-const handleSignInWithGoogle = () => {
-    const provider = new GoogleAuthProvider();
-
-    signInWithPopup(auth, provider)
-        .then((userCredential) => {
-            // User signed in with Google successfully
-            const user = userCredential.user;
-            console.log('User signed in with Google:', user);
-
-            // Redirect the user to a different page
-            window.location.href = './bem-vindo.html'; // Replace with the desired destination
-        })
-        .catch((error) => {
-            // Handle Google sign-in errors
-            console.error('Error signing in with Google:', error);
-        });
+// Function to register a new user with Email/Password
+const registerWithEmailAndPassword = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
 };
+
+export { signInWithEmailAndPassword, registerWithEmailAndPassword };
